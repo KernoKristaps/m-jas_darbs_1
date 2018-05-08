a317ed1b628585dc3e46efb9c85b43b18306e9b0
KernoKristaps <39062321+KernoKristaps@users.noreply.github.com>
2018-05-08 18:32:16 +0300

    Pats_mājās
    Pirmo mājas darbu

--- mājas_darbs_1/Program.cs
+++ mājas_darbs_1/Program.cs
@@ -10,6 +10,27 @@ namespace mājas_darbs_1
     {
         static void Main(string[] args)
         {
+            int rezultāts = Aprēķins();
+            Console.WriteLine("c = " + rezultāts);
+            Console.ReadLine();
+            int a = 5;
+            int b = 6;
+
+            int c = a + b;
+            a = 5;
+            b = 6;
+            c = a + b;
+            Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, c);
+            Console.ReadLine();
+        }
+        static int Aprēķins() //funkcija, kas atgriež vērtību
+        {
+            int a = 5;
+            int b = 6;
+
+            int c = a + b;
+
+            return c;
         }
     }
 }
