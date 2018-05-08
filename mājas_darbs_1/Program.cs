34ab9a4e666b1497e3b04524b0edc151e80be6b2
KernoKristaps <39062321+KernoKristaps@users.noreply.github.com>
2018-05-08 16:45:39 +0300

    Add project files.

---
+++ mājas_darbs_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mājas_darbs_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
